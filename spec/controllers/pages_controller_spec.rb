34bbc5cbac2673731d6fe4d10b5d59f637c71122
Vishal25 <vishalchauthmal@gmail.com>
2014-05-08 12:51:32 +0530

    Modified the titles

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,21 @@
 require 'spec_helper'
 
+render_views
+
 describe PagesController do
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Ruby on Rails Sample App | Home")
+    end
+    it "should have non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -14,12 +24,20 @@ describe PagesController do
       get 'contact'
       response.should be_success
     end
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => "Ruby on Rails Sample App | Contact")
+    end
   end
 
   describe "GET 'about' " do
     it "returns http success" do
-      git 'about'
+      get 'about'
       response.should be_success
+    end
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => "Ruby on Rails Sample App | About")
     end
   end  
 
