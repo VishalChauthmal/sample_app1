8cbacd396ba9aad0aafce8c5b3515ecf7d8f4da9
Vishal25 <vishalchauthmal@gmail.com>
2014-05-10 12:28:43 +0530

    Added a title helper and stylesheets

---
+++ config/initializers/assets.rb
@@ -0,0 +1,2 @@
+Rails.application.config.assets.precompile += %w( blueprint/screen.css )
+Rails.application.config.assets.precompile += %w( blueprint/print.css )
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,8 +1,12 @@
 require 'spec_helper'
 
-render_views
-
 describe PagesController do
+
+  render_views
+
+  before(:each) do
+    @base_title = "Ruby on Rails Sample App"
+  end
 
   describe "GET 'home'" do
     it "returns http success" do
@@ -11,7 +15,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "Ruby on Rails Sample App | Home")
+      response.should have_selector("title", :content => "#{@base_title} | Home")
     end
     it "should have non-blank body" do
       get 'home'
@@ -26,7 +30,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "Ruby on Rails Sample App | Contact")
+      response.should have_selector("title", :content => "#{@base_title} | Contact")
     end
   end
 
@@ -37,7 +41,7 @@ describe PagesController do
     end
     it "should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "Ruby on Rails Sample App | About")
+      response.should have_selector("title", :content => "#{@base_title} | About")
     end
   end  
 
