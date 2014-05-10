2fde320ee589bef3f9afc5a1fb00640fd02a0d38
Vishal25 <vishalchauthmal@gmail.com>
2014-05-10 20:45:40 +0530

    Added a custom stylesheet

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,2 +1,3 @@
 Rails.application.config.assets.precompile += %w( blueprint/screen.css )
 Rails.application.config.assets.precompile += %w( blueprint/print.css )
+Rails.application.config.assets.precompile += %w( custom.css )
